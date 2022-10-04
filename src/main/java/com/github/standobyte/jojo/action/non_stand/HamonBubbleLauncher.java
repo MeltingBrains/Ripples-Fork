5061821cf6440a3114b274fd2959bc6e8ff48e18
MeltingBrains <83098794+MeltingBrains@users.noreply.github.com>
2022-10-05 00:00:54 +0200

    Update HamonBubbleLauncher.java

--- src/main/java/com/github/standobyte/jojo/action/non_stand/HamonBubbleLauncher.java
+++ src/main/java/com/github/standobyte/jojo/action/non_stand/HamonBubbleLauncher.java
@@ -1,8 +1,8 @@
-package com.github.standobyte.jojo.action.non_stand;
+package com.github.standobyte.jojo.action.stand;
 
 import com.github.standobyte.jojo.action.ActionTarget;
 import com.github.standobyte.jojo.entity.damaging.projectile.HamonBubbleEntity;
-import com.github.standobyte.jojo.power.nonstand.INonStandPower;
+import com.github.standobyte.jojo.power.stand.IStandPower;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.world.World;
@@ -14,12 +14,12 @@ public class HamonBubbleLauncher extends HamonAction {
     }
     
     @Override
-    protected void holdTick(World world, LivingEntity user, INonStandPower power, int ticksHeld, ActionTarget target, boolean requirementsFulfilled) {
+    protected void holdTick(World world, LivingEntity user, IStandPower power, int ticksHeld, ActionTarget target, boolean requirementsFulfilled) {
         if (requirementsFulfilled && !world.isClientSide()) {
             for (int i = 0; i < 4; i++) {
                 HamonBubbleEntity bubbleEntity = new HamonBubbleEntity(user, world);
-                float velocity = 0.1F + user.getRandom().nextFloat() * 0.5F;
-                bubbleEntity.shootFromRotation(user, velocity, 16.0F);
+                float velocity = 0.1F + user.getRandom().nextFloat() * 0.35F;
+                bubbleEntity.shootFromRotation(user, velocity, 20.0F);
                 world.addFreshEntity(bubbleEntity);
             }
         }
